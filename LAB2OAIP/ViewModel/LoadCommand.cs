9b386101c8094360f7c28ec7eeaf98c58cde79d3
Egor Golovko <bumbox06@mail.ru>
2026-03-04 14:54:25 +0700

    Добавление файла LoadCommand

---
+++ LAB2OAIP/ViewModel/LoadCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class LoadCommand : ICommand
+{
+	public LoadCommand()
+	{
+
+         private readonly MainVM _vm;
+
+    public LoadCommand(MainVM vm)
+    {
+        _vm = vm;
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter) => true;
+
+    public void Execute(object? parameter)
+    {
+        _vm.Load();
+    }
+}
+}
