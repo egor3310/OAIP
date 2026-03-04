86ee530c72f86b06fed43f32a960cba09aee4f05
Egor Golovko <bumbox06@mail.ru>
2026-03-04 14:48:55 +0700

    Добавил класс Contact

---
+++ LAB2OAIP/Model/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Contact
+{
+	public Contact()
+	{
+
+        public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+
+    public Contact()
+    {
+        Name = string.Empty;
+        PhoneNumber = string.Empty;
+        Email = string.Empty;
+    }
+
+    public Contact(string name, string phoneNumber, string email)
+    {
+        Name = name;
+        PhoneNumber = phoneNumber;
+        Email = email;
+    }
+}
+}
