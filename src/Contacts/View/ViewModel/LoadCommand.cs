0f2808657d198fd429ad19cf054d53243e825c2d
Egor Golovko <bumbox06@mail.ru>
2026-03-17 23:51:52 +0700

    Добавил 2 файла SaveCommand и LoadCommand

---
+++ src/Contacts/View/ViewModel/LoadCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace View.ViewModel
+{
+    public class LoadCommand : ICommand
+    {
+        private readonly MainVM _vm;
+
+        public LoadCommand(MainVM vm) => _vm = vm;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter) => true;
+
+        public void Execute(object? parameter)
+        {
+            _vm.Load();
+        }
+    }
+}
