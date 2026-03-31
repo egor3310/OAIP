9ee1216aa5b53c27444100fa16c9dc073381fa4b
Egor Golovko <bumbox06@mail.ru>
2026-03-31 23:05:21 +0700

    Исправленные файлы с командами

---
+++ src/Contacts/View/ViewModel/ApplyCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Input;
+using View.ViewModel;
+
+namespace View.ViewModel
+{
+
+    /// <summary>
+    /// Команда для применения изменений при создании или редактировании контакта.
+    /// </summary>
+    public class ApplyCommand : ICommand
+    {
+        private readonly MainVM _vm;
+
+        public ApplyCommand(MainVM vm)
+        {
+            _vm = vm;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return _vm.IsApplyVisible;
+        }
+
+        /// <summary>
+        /// Команда для применения изменений при создании или редактировании контакта.
+        /// </summary>
+        public void Execute(object? parameter)
+        {
+            _vm.Apply();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ src/Contacts/View/ViewModel/EditCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+using View.ViewModel;
+
+namespace View.ViewModel
+{
+
+    /// <summary>
+    /// Команда для перехода в режим редактирования выбранного контакта.
+    /// </summary>
+    public class EditCommand : ICommand
+    {
+        private readonly MainVM _vm;
+
+        public EditCommand(MainVM vm)
+        {
+            _vm = vm;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return _vm.CanEdit;
+        }
+
+        /// <summary>
+        /// Выполняет переход в режим редактирования контакта.
+        /// </summary>
+        /// <param name="parameter">Параметр команды.</param>
+        public void Execute(object? parameter)
+        {
+            _vm.Edit();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
